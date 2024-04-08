1ce21353a0a33868e6ad74724cda0c7439bf93db
Veronica <vgalbicka@gmail.com>
2024-04-07 23:44:47 -0300

    add

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,11 +3,22 @@ public class Pelicula {
     int anioLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaEvaluaciones;
+    int cantidadDeEvaluaciones;
 
     void mostrarFichaTecnica(){
-        System.out.println("Ficha Técnica");
+        System.out.println("\n-*-*-*-*-*-*\nFicha Técnica\n-*-*-*-*-*-*\n");
         System.out.println("Título: " + this.nombre);
         System.out.println("Año de lanzamiento es: " + this.anioLanzamiento);
         System.out.println("Duración: " + this.duracionEnMinutos + " min.");
+    }
+
+    void evaluar(double nota){
+        sumaEvaluaciones += nota;
+        cantidadDeEvaluaciones++;
+    }
+
+    double calcularPromedio(){
+        return sumaEvaluaciones / cantidadDeEvaluaciones;
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,20 +5,30 @@ public class Principal {
         miPelicula.anioLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
 
-
-        System.out.println("Mi pelicula es: " + miPelicula.nombre);
-        System.out.println("Año de lanzamiento es: " + miPelicula.anioLanzamiento);
-
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
         otraPelicula.anioLanzamiento = 1999;
         otraPelicula.duracionEnMinutos = 110;
 
-        System.out.println("Mi pelicula es: " + otraPelicula.nombre);
-        System.out.println("Año de lanzamiento es: " + otraPelicula.anioLanzamiento);
-        System.out.println("-*-*-*-*-*-*");
         miPelicula.mostrarFichaTecnica();
-        otraPelicula.mostrarFichaTecnica();
+        miPelicula.evaluar(7.3);
+        miPelicula.evaluar(0.5);
+        miPelicula.evaluar(6.5);
+        miPelicula.evaluar(7.3);
+        miPelicula.evaluar(9.5);
+        miPelicula.evaluar(6.5);
 
+        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" ,miPelicula.sumaEvaluaciones));
+        System.out.println("El promedio de notas evaluadas es: " + String.format("%.2f" , miPelicula.calcularPromedio()));
+        System.out.println("Cantidad de evaluaciones contempladas: " + miPelicula.cantidadDeEvaluaciones);
+
+        otraPelicula.mostrarFichaTecnica();
+        otraPelicula.evaluar(9.9);
+        otraPelicula.evaluar(8.8);
+        otraPelicula.evaluar(8.3);
+
+        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" , otraPelicula.sumaEvaluaciones));
+        System.out.println("El promedio de notas evaluadas es: " + String.format("%.2f", otraPelicula.calcularPromedio()));
+        System.out.println("Cantidad de evaluaciones contempladas: " + otraPelicula.cantidadDeEvaluaciones);
     }
 }
