0f005331f0f08251fbe962e7388f1cda90957ac9
Veronica <vgalbicka@gmail.com>
2024-04-08 00:16:00 -0300

    Encapsulamiento y modificadores de acceso

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,10 +1,61 @@
 public class Pelicula {
-    String nombre;
-    int anioLanzamiento;
-    int duracionEnMinutos;
-    boolean incluidoEnElPlan;
-    double sumaEvaluaciones;
-    int cantidadDeEvaluaciones;
+    private String nombre;
+    private int anioLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaEvaluaciones;
+    private int cantidadDeEvaluaciones;
+
+    public Pelicula() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioLanzamiento() {
+        return anioLanzamiento;
+    }
+
+    public void setAnioLanzamiento(int anioLanzamiento) {
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    public int getDuracionEnMinutos(int i) {
+        return duracionEnMinutos;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public double getSumaEvaluaciones() {
+        return sumaEvaluaciones;
+    }
+
+    public void setSumaEvaluaciones(double sumaEvaluaciones) {
+        this.sumaEvaluaciones = sumaEvaluaciones;
+    }
+
+    public int getCantidadDeEvaluaciones() {
+        return cantidadDeEvaluaciones;
+    }
+
+    public void setCantidadDeEvaluaciones(int cantidadDeEvaluaciones) {
+        this.cantidadDeEvaluaciones = cantidadDeEvaluaciones;
+    }
 
     void mostrarFichaTecnica(){
         System.out.println("\n-*-*-*-*-*-*\nFicha Técnica\n-*-*-*-*-*-*\n");
--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,14 @@
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.anioLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setAnioLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
 
         Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.anioLanzamiento = 1999;
-        otraPelicula.duracionEnMinutos = 110;
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setAnioLanzamiento(1999);
+        otraPelicula.setDuracionEnMinutos(110);
 
         miPelicula.mostrarFichaTecnica();
         miPelicula.evaluar(7.3);
@@ -18,17 +18,17 @@ public class Principal {
         miPelicula.evaluar(9.5);
         miPelicula.evaluar(6.5);
 
-        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" ,miPelicula.sumaEvaluaciones));
+        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" , miPelicula.getSumaEvaluaciones()));
         System.out.println("El promedio de notas evaluadas es: " + String.format("%.2f" , miPelicula.calcularPromedio()));
-        System.out.println("Cantidad de evaluaciones contempladas: " + miPelicula.cantidadDeEvaluaciones);
+        System.out.println("Cantidad de evaluaciones contempladas: " + miPelicula.getCantidadDeEvaluaciones());
 
         otraPelicula.mostrarFichaTecnica();
         otraPelicula.evaluar(9.9);
         otraPelicula.evaluar(8.8);
         otraPelicula.evaluar(8.3);
 
-        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" , otraPelicula.sumaEvaluaciones));
+        System.out.println("La suma de las evaluaciones es: " + String.format("%.2f" , otraPelicula.getSumaEvaluaciones()));
         System.out.println("El promedio de notas evaluadas es: " + String.format("%.2f", otraPelicula.calcularPromedio()));
-        System.out.println("Cantidad de evaluaciones contempladas: " + otraPelicula.cantidadDeEvaluaciones);
+        System.out.println("Cantidad de evaluaciones contempladas: " + otraPelicula.getCantidadDeEvaluaciones());
     }
 }
