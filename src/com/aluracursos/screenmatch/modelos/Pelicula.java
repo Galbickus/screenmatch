b1a1aec6c4d419d498561bb97238fa0d6dc59146
Veronica <vgalbicka@gmail.com>
2024-04-10 12:47:48 -0300

    Herencia

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,66 +1,13 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int anioLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnElPlan;
-    private double sumaEvaluaciones;
-    private int cantidadDeEvaluaciones;
+public class Pelicula extends Titulo{
+    private String director;
 
-    public String getNombre() {
-        return nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getAnioLanzamiento() {
-        return anioLanzamiento;
-    }
-
-    public void setAnioLanzamiento(int anioLanzamiento) {
-        this.anioLanzamiento = anioLanzamiento;
-    }
-
-    public int getDuracionEnMinutos(int i) {
-        return duracionEnMinutos;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnElPlan() {
-        return incluidoEnElPlan;
-    }
-
-    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-        this.incluidoEnElPlan = incluidoEnElPlan;
-    }
-
-    public double getSumaEvaluaciones() {
-        return sumaEvaluaciones;
-    }
-
-    public int getCantidadDeEvaluaciones() {
-        return cantidadDeEvaluaciones;
-    }
-
-    public void mostrarFichaTecnica(){
-        System.out.println("\n-*-*-*-*-*-*\nFicha Técnica\n-*-*-*-*-*-*\n");
-        System.out.println("Título: " + this.nombre);
-        System.out.println("Año de lanzamiento es: " + this.anioLanzamiento);
-        System.out.println("Duración: " + this.duracionEnMinutos + " min.");
-    }
-
-    public void evaluar(double nota){
-        sumaEvaluaciones += nota;
-        cantidadDeEvaluaciones++;
-    }
-
-    public double calcularPromedio(){
-        return sumaEvaluaciones / cantidadDeEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private int duracion;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+}
