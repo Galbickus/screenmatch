de2099c7d803edb700135d772b3dec560beea4a4
Veronica <vgalbicka@gmail.com>
2024-04-19 23:34:13 -0300

    change

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -13,6 +13,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 /*interfaz pública HttpResponse<T>
 Una respuesta HTTP.
@@ -29,48 +31,59 @@ La clase BodyHandlersproporciona implementaciones de muchos controladores de res
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Escriba el nombre de una película");
-        var busqueda = lectura.nextLine();
-        String direccion = "https://www.omdbapi.com/?t=" + busqueda.replace(" ", "+") + "&apikey=26dd704c";
-        try{
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(direccion))
-                    .build();
-            /*HttpRequest req = new HttpRequest();
-             * no se puede hacer un new de HttpRequest, porque es una clase abstrac-
-             * ta, por eso usamos un patron Builder*/
-            /**/
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        List<Titulo> titulos = new ArrayList<>();
 
-            String json = response.body();
+        while (true){
+            System.out.println("Escriba el nombre de una película");
+            var busqueda = lectura.nextLine();
+
+            if (busqueda.equalsIgnoreCase("salir")){
+                break;
+            }
+
+            String direccion = "https://www.omdbapi.com/?t=" + busqueda.replace(" ", "+") + "&apikey=26dd704c";
+            try{
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                /*HttpRequest req = new HttpRequest();
+                 * no se puede hacer un new de HttpRequest, porque es una clase abstrac-
+                 * ta, por eso usamos un patron Builder*/
+                /**/
+
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
 
 
-            System.out.println(json);
+                System.out.println(json);
 
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(miTituloOmdb);
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                        .create();
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(miTituloOmdb);
 
-            Titulo miTitulo = new Titulo(miTituloOmdb);
-            System.out.println("Título ya convertido: " + miTitulo);
+                Titulo miTitulo = new Titulo(miTituloOmdb);
+                System.out.println("Título ya convertido: " + miTitulo);
 
-            FileWriter escritura = new FileWriter("peliculas.txt");
-            escritura.write(miTitulo.toString());
-            escritura.close();
+                FileWriter escritura = new FileWriter("peliculas.txt");
+                escritura.write(miTitulo.toString());
+                escritura.close();
 
-        }catch(NumberFormatException e){
-            System.out.println("Ocurrió un error: ");
-            System.out.println(e.getMessage());
-        }catch (IllegalArgumentException ie){
-            System.out.println("Error en la URI, verifique la dirección.");
-        }catch (ErrorEnConversionDeDuracionException e){
-            System.out.println(e.getMessage());
+            }catch(NumberFormatException e){
+                System.out.println("Ocurrió un error: ");
+                System.out.println(e.getMessage());
+            }catch (IllegalArgumentException ie){
+                System.out.println("Error en la URI, verifique la dirección.");
+            }catch (ErrorEnConversionDeDuracionException e){
+                System.out.println(e.getMessage());
+            }
         }
+
         System.out.println("Finalizó la ejecución del programa");
     }
 }
